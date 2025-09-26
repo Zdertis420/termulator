2fccb058fb0e7341c2dfc5dd051eb195c9ad434d
Zdertis420 <AnRobo07@yandex.ru>
2025-09-26 22:09:40 +0300

    Сделал тему TokyoNight Storm, но поле ввода не перекрашивается ):

--- src/nativeMain/kotlin/orc/zdertis420/termulator/Main.kt
+++ src/nativeMain/kotlin/orc/zdertis420/termulator/Main.kt
@@ -1,15 +1,18 @@
 package orc.zdertis420.termulator
 
+import org.gtkkn.bindings.gdk.Display
 import org.gtkkn.bindings.gio.ApplicationFlags
 import org.gtkkn.bindings.gtk.Application
 import org.gtkkn.bindings.gtk.ApplicationWindow
 import org.gtkkn.bindings.gtk.Box
+import org.gtkkn.bindings.gtk.CssProvider
 import org.gtkkn.bindings.gtk.Entry
 import org.gtkkn.bindings.gtk.Orientation
+import org.gtkkn.bindings.gtk.StyleContext
 import org.gtkkn.extensions.gio.runApplication
 import org.gtkkn.extensions.glib.util.log.Log
 import org.gtkkn.extensions.glib.util.log.writer.installConsoleLogWriter
-import org.gtkkn.extensions.gtk.setMargins
+import org.gtkkn.native.gtk.GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
 
 fun main() {
     Log.installConsoleLogWriter()
@@ -20,16 +23,41 @@ fun main() {
         window.title = "GTK: Ввод и Консоль (Только Код)"
         window.setDefaultSize(350, 150)
 
-        val box = Box(Orientation.VERTICAL, 12)
+        val cssProvider = CssProvider()
+        cssProvider.loadFromPath("src/nativeMain/resources/style.css")
 
-        box.setMargins(20)
+        val display = Display.getDefault()
+        StyleContext.addProviderForDisplay(
+            display!!,
+            cssProvider,
+            GTK_STYLE_PROVIDER_PRIORITY_APPLICATION.toUInt()
+        )
+
+        val mainBox = Box(Orientation.VERTICAL, 0)
+
+        val margin = 12
+
+        val contentArea = Box(Orientation.VERTICAL, 0)
+        contentArea.vexpand = true
+        contentArea.marginTop = margin
+        contentArea.marginStart = margin
+        contentArea.marginEnd = margin
+
+        val inputBox = Box(Orientation.VERTICAL, 0)
+
+        inputBox.marginStart = margin
+        inputBox.marginEnd = margin
+        inputBox.marginBottom = margin
 
         val entry = Entry()
-        entry.placeholderText = "Введите текст и нажмите Enter"
+        entry.placeholderText = "Введите команду и нажмите Enter..."
 
-        box.append(entry)
+        inputBox.append(entry)
 
-        window.child = box
+        mainBox.append(contentArea)
+        mainBox.append(inputBox)
+
+        window.child = mainBox
 
         val handleInput = {
             val text: String = entry.getText()
